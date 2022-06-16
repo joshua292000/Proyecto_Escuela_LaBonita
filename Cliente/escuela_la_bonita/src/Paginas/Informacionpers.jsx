3ef0fb66b6b95026ca2ca87e84f31c590300b930
Joss1015 <jossems.1012@gmail.com>
2022-06-16 17:32:42 -0600

    Inicio_interfaz

---
+++ Cliente/escuela_la_bonita/src/Paginas/Informacionpers.jsx
@@ -0,0 +1,11 @@
+import { ButtonSiguiente, TXT_info } from "../Componentes/Utils";
+export default function Inicio() {
+    return (
+      <div>
+        <h1>Información personal del estudiante</h1>
+        <label>Cédula:</label>
+        <TXT_info name="txt_cedula" id="txt_cedula"></TXT_info>
+        <ButtonSiguiente dir="informacionest" nom="Siguiente" css="button_Siguiente"/>
+      </div>
+    );
+  }
