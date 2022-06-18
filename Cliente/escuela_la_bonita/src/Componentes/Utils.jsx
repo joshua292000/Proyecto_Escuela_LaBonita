2f18861fe2f41ae3a35323ea7be0b3f8f33b0a58
KevinMora14 <86388845+KevinMora14@users.noreply.github.com>
2022-06-17 23:43:37 -0600

    Update Utils.jsx

--- Cliente/escuela_la_bonita/src/Componentes/Utils.jsx
+++ Cliente/escuela_la_bonita/src/Componentes/Utils.jsx
@@ -24,67 +24,83 @@ export function InfoPeronal(){
     return(
          <div>
             <table width="40%">
-                <tr>
-                <td><label>Cédula:</label><TXT_info name="txt_cedula" id="txt_cedula"></TXT_info><br></br></td><br></br>
-                <tr><Button label="Buscar"  /><br></br></tr>
-                <td><label>Fecha nacimiento:</label><br></br><input type="date" name="fnacimiento" id="fnacimiento"></input><br></br><br></br></td>
-                </tr>
-                <tr>
-                <td><label>Primer nombre:</label><TXT_info name="txt_pnombre" id="txt_pnombre"></TXT_info><br></br></td>
-                <td><label>Segundo nombre:</label><TXT_info name="txt_snombre" id="txt_snombre"></TXT_info><br></br></td>
-                <td><label>Primer apellido:</label><TXT_info name="txt_papellido" id="txt_papellido"></TXT_info><br></br></td>
-                <td> <label>Segundo apellido:</label><TXT_info name="txt_papellido" id="txt_papellido"></TXT_info><br></br></td>
- 
-                </tr>
-                <tr>
-                <td>
-                <div>
-                <label>Provincia:</label>
-                <select name="Provincia" id="Provincia">
-                    <option value="SanJose">San José</option>
-                    <option value="Alajuela">Alajuela</option>
-                    <option value="Heredia">Heredia</option>
-                    <option value="Puntarenas">Puntarenas</option>
-                    <option value="Cartago">Cartago</option>
-                    <option value="Limon">Limón</option>
-                    <option value="Guanacaste">Guanacaste</option>
-                </select>
-                </div><br /><br></br></td>
-                <td>
-                <div>
-                <label>Cantón:</label>
-                <select name="Canton" id="Canton">
-                    <option value="PZ">Pérez Zeledón</option>
-                    <option value="Escazu">Escazú</option>
-                    <option value="Heredia">Desamparados</option>
-                </select>
-                </div><br /><br></br></td>
-                <td>
-                <div>
-                <label>Distrito:</label>
-                <select name="Distrito" id="Distrito">
-                    <option value="SanIsidro">San Isidro de El General</option>
-                    <option value="ElGeneral">El General</option>
-                    <option value="DanielFlores">Daniel Flores</option>
-                </select>
-                </div><br /><br></br></td> 
-                <br></br>
-                </tr>
-                <td><label>Sexo:</label>
-                <input type="radio" id="hombre" name="sexoest" value="soltero"></input>
-                <label for="hombre">Hombre</label>
-                <input type="radio" id="mujer" name="sexoest" value="Mujer"></input>
-                <label for="mujer">Mujer </label><br></br><br></br></td>
-                <td></td>
-                <td>
-                <div>
-                <label>Lugar nacimiento:</label>
-                <select name="lugarnacimiento" id="lugarnacimiento">
-                    <option value="CostaRica">Costa Rica</option>
-                    <option value="Panamá">Panamá</option>
-                    <option value="USA">Estados Unidos</option>
-                </select>
-                </div><br /><br></br></td>
+            <tbody>
+                    <tr>
+                        <td><label>Cédula:</label><TXT_info name="txt_cedula" id="txt_cedula"></TXT_info><br></br></td><br></br>
+                        <tr><Button label="Buscar"  /><br></br></tr>
+                        <td><label>Fecha nacimiento:</label><br></br><input type="date" name="fnacimiento" id="fnacimiento"></input><br></br><br></br></td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td><label>Primer nombre:</label><TXT_info name="txt_pnombre" id="txt_pnombre"  ></TXT_info><br></br></td>
+                        <td><label>Segundo nombre:</label><TXT_info name="txt_snombre" id="txt_snombre"></TXT_info><br></br></td>
+                        <td><label>Primer apellido:</label><TXT_info name="txt_papellido" id="txt_papellido"></TXT_info><br></br></td>
+                        <td> <label>Segundo apellido:</label><TXT_info name="txt_papellido" id="txt_papellido"></TXT_info><br></br></td>
+                   </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td>
+                            <div>
+                            <label>Provincia:</label>
+                            <select name="Provincia" id="Provincia">
+                                <option value="SanJose">San José</option>
+                                <option value="Alajuela">Alajuela</option>
+                                <option value="Heredia">Heredia</option>
+                                <option value="Puntarenas">Puntarenas</option>
+                                <option value="Cartago">Cartago</option>
+                                <option value="Limon">Limón</option>
+                                <option value="Guanacaste">Guanacaste</option>
+                            </select>
+                            </div><br /><br></br>
+                        </td>
+                        <td>
+                            <div>
+                            <label>Cantón:</label>
+                            <select name="Canton" id="Canton">
+                                <option value="PZ">Pérez Zeledón</option>
+                                <option value="Escazu">Escazú</option>
+                                <option value="Heredia">Desamparados</option>
+                            </select>
+                            </div><br /><br></br></td>
+                            <td>
+                            <div>
+                            <label>Distrito:</label>
+                            <select name="Distrito" id="Distrito">
+                                <option value="SanIsidro">San Isidro de El General</option>
+                                <option value="ElGeneral">El General</option>
+                                <option value="DanielFlores">Daniel Flores</option>
+                            </select>
+                            </div><br /><br></br>
+                        </td>
+                        <br></br>
+                   </tr>
+                </tbody>
+                <tbody>
+                        <td>
+                            <label>Sexo:</label>
+                            <input type="radio" id="hombre" name="sexoest" value="soltero"></input>
+                            <label htmlFor="hombre">Hombre</label>
+                            <input type="radio" id="mujer" name="sexoest" value="Mujer"></input>
+                            <label htmlFor="mujer">Mujer </label>
+                            <br></br>
+                            <br></br>
+                        </td>
+                        <td></td>
+                        <td>
+                            <div>
+                            <label>Lugar nacimiento:</label>
+                            <select name="lugarnacimiento" id="lugarnacimiento">
+                                <option value="CostaRica">Costa Rica</option>
+                                <option value="Panamá">Panamá</option>
+                                <option value="USA">Estados Unidos</option>
+                            </select>
+                            </div><br /><br></br>
+                        </td>
+                    
+                </tbody>
+                
             </table>
         </div>
     )
@@ -93,103 +109,120 @@ export function InfoPeronal(){
 export function InfoEncargado(){
     return(
          <div>
-            <table>
-                <tr>
-                    <td>
-                        <label >Informacion Laboral:</label>
-                    </td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td><label>Ocupación:</label></td>
-                    <td><label>Lugar de Trabajo:</label></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td> <TXT_info name="txt_Ocupación" id="txt_Ocupación"></TXT_info><br /></td>
-                    <td><TXT_info name="txt_LTrabajo" id="txt_LTrabajo"></TXT_info><br /></td> 
-                </tr>
-                <tr>
-                    <td><label>Contacto:</label></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td><label>Correo Electronico:</label><br /></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td><TXT_info name="txt_CElectronico" id="txt_CElectronico"></TXT_info><br /></td>
-                    <td></td>
-                    
-                    
-                </tr>
-                <tr>
-                    <td></td>
-                    <td><label>Número de Teléfono:</label><br /></td>
-                    <td></td>
-                    
-                </tr>
-                <tr>
-                    <td></td>
-                    <td><TXT_info name="txt_NumTelefono" id="txt_NumTelefono"></TXT_info><br /></td>
-                    <td></td>
-                    
-                </tr>
-                <tr>
-                
-                </tr>
-                <tr>
-                    <td><label>Parentesco:</label></td>
-                    <td></td>
-                    <td><label>Escolaridad:</label></td>
-                </tr>
-                
-                <tr>
-                    <td></td>
-                    <td>
+            <table width="40%">
+                <tbody>
+                    <tr>
+                        <td>
+                            <label >Informacion Laboral:</label>
+                        </td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td><label>Ocupación:</label></td>
+                        <td><label>Lugar de Trabajo:</label></td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td> <TXT_info name="txt_Ocupación" id="txt_Ocupación"></TXT_info></td>
+                        <td><TXT_info name="txt_LTrabajo" id="txt_LTrabajo"></TXT_info></td> 
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td><label>Contacto:</label></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td><label>Correo Electronico:</label></td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td><TXT_info name="txt_CElectronico" id="txt_CElectronico"></TXT_info></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td><label>Número de Teléfono:</label></td>
+                        <td></td>
+                        
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td><TXT_info name="txt_NumTelefono" id="txt_NumTelefono"></TXT_info></td>
+                        <td></td>
+                        
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td><label>Parentesco:</label></td>
+                        <td></td>
+                        <td><label>Escolaridad:</label></td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <div >
+                            <select name="Parentesco" id="Parentesco">
+                                <option value="Madre">Madre</option>
+                                <option value="Abuelo">Abuelo(a)</option>
+                                <option value="Tío">Tío(a)</option>
+                                <option value="Hermano">Hermano(a)</option>
+                                <option value="Madrastra">Madrastra</option>
+                                <option value="Padre">Padre</option>   
+                                <option value="Padrastro">Padrastro</option> 
+                                <option value="EncargadoLegal">Encargad(a) legal</option>
+                            </select>
+                            </div>
+                        </td>
+                        <td></td>
+                        <td>
                         <div >
-                        <select name="Parentesco" id="Parentesco">
-                            <option value="Madre">Madre</option>
-                            <option value="Abuelo">Abuelo(a)</option>
-                            <option value="Tío">Tío(a)</option>
-                            <option value="Hermano">Hermano(a)</option>
-                            <option value="Madrastra">Madrastra</option>
-                            <option value="Padre">Padre</option>   
-                            <option value="Padrastro">Padrastro</option> 
-                            <option value="EncargadoLegal">Encargad(a) legal</option>
-                        </select>
-                        </div>
-                    </td>
-                    <td></td>
-                    <td>
-                    <div >
-                        <select name="Escolaridad" id="Escolaridad">
-                        <option value="Ninguna">Ninguna</option>
-                        <option value="Primaria incompleta">Primaria incompleta</option>
-                        <option value="Primaria completa">Primaria completa</option>
-                        <option value="Secundaria incompleta">Secundaria incompleta</option>
-                        <option value="Secundaria completa">Secundaria completa</option>
-                        <option value="Técnico profesional">Técnico profesional</option>   
-                        <option value="Universitaria">Universitaria</option> 
-                        </select>
-                        </div>
-                    </td>
-                </tr>
-                <tr></tr>
-                <tr>
-                <td></td>
-                    <td><label>Vive con la (el) estudiante:</label><br /></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td> 
-                    <input type="radio" id="SI" name="viveest" value="Si"></input>
-                    <label for="Si">Si</label>
-                    <input type="radio" id="No" name="viveest" value="No"></input>
-                    <label for="No">No</label><br></br><br />
-                    </td>
-                </tr>
+                            <select name="Escolaridad" id="Escolaridad">
+                            <option value="Ninguna">Ninguna</option>
+                            <option value="Primaria incompleta">Primaria incompleta</option>
+                            <option value="Primaria completa">Primaria completa</option>
+                            <option value="Secundaria incompleta">Secundaria incompleta</option>
+                            <option value="Secundaria completa">Secundaria completa</option>
+                            <option value="Técnico profesional">Técnico profesional</option>   
+                            <option value="Universitaria">Universitaria</option> 
+                            </select>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td><label>Vive con la (el) estudiante:</label></td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td> 
+                        <input type="radio" id="SI" name="viveest" value="Si"></input>
+                        <label htmlFor="Si">Si</label>
+                        <input type="radio" id="No" name="viveest" value="No"></input>
+                        <label htmlFor="No">No</label>
+                        </td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     )
