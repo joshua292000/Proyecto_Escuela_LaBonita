314f922a0bf80b797a5c932261fc5665cf66af7d
KevinMora14 <86388845+KevinMora14@users.noreply.github.com>
2022-06-26 13:01:39 -0600

    libreria

--- Cliente/escuela_la_bonita/src/App.js
+++ Cliente/escuela_la_bonita/src/App.js
@@ -4,6 +4,9 @@ import {InfoEstudiante} from "./Paginas/InformacionEst";
 import InformacionEnc from "./Paginas/InformacionEnc";  
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from 'react';
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";   
 
 export function App() {
 return(
