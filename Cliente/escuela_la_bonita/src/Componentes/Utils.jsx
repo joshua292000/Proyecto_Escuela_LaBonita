e65d57254df6382b2acbc9881c58ff9259b5eca4
KevinMora14 <86388845+KevinMora14@users.noreply.github.com>
2023-01-05 21:06:48 -0600

    Menu principal

--- Cliente/escuela_la_bonita/src/Componentes/Utils.jsx
+++ Cliente/escuela_la_bonita/src/Componentes/Utils.jsx
@@ -18,3 +18,4 @@ export function ButtonSiguiente(props) {
     </div>
   );
 }
+
--- Cliente/escuela_la_bonita/src/Paginas/Inicio.jsx
+++ Cliente/escuela_la_bonita/src/Paginas/Inicio.jsx
@@ -11,11 +11,22 @@ import RegistroFun from '../Recursos/RegistrarFuncionario.png'
 import ReporteComedor from '../Recursos/reporteComedor.png'
 import { Header } from "../Componentes/Cabecera";
 import React, { Component } from 'react';
+import { Card } from 'primereact/card';
+import { within } from "@testing-library/react";
+import { Tooltip } from 'primereact/tooltip';
 
 export default function Inicio() {
   const cookies = new Cookies();
   const navegar = useNavigate();
-
+  const header2 = (
+    <img alt="Card" src="images/usercard.png" onError={(e) => e.target.src = 'https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png'} />
+  );
+  const footer = (
+    <span>
+      {/*<Button label="Save" icon="pi pi-check" />
+      <Button label="Cancel" icon="pi pi-times" className="p-button-secondary ml-2" />*/}
+    </span>
+  );
   const VerEle = () => {
 
     const IdDirector = cookies.get('Rol_Id');
@@ -23,12 +34,19 @@ export default function Inicio() {
     if (IdDirector === "1") {
       console.log("ver ele 2", cookies.get('Rol_Id'))
       return (
-        <div id="RegistrarFuncionarios">
-          <img src={RegistroFun} alt="Escuela Rodrigo Facio Brenes" width="150%"
-            onClick={() => navegar("/RegistroProfesor")}
-          />
-            <h3>Registrar funcionario</h3>
-          
+        <div>
+          <Tooltip target=".Funcionarios" position="bottom" mouseTrack mouseTrackLeft={10}>
+                  <label>Sistema de registro y actualización de funcionarios</label>
+          </Tooltip>
+
+          <Card
+            className="Funcionarios"
+            id="RegistrarFuncionarios"
+            style={{ width: '20em' }}
+            header={<img alt="Card" src={RegistroFun} />}
+            onClick={() => navegar("/RegistroProfesor")}>
+            <h3>Registrar funcionarios</h3>
+          </Card>
         </div>
       )
     }
@@ -40,61 +58,84 @@ export default function Inicio() {
       <Header />
       <div className="Div">
         <h1>Página de inicio</h1>
-        <div className="container" style={{ paddingLeft:'5%', borderRadius: '15px', border: '15px solid rgb(163, 29, 29, 0.06)' }}  >
+        <div className="container" style={{ paddingLeft: '5%', borderRadius: '15px', border: '15px solid rgb(163, 29, 29, 0.06)' }}  >
           <br />
-          <div className="row justify-content-md-center" >
-            <div className="col-sm ">
-              <div id="Matricula">
-                <img
-                  src={Matricula}
-                  alt="Matricula"
-                  width="80%"
-                  onClick={() => navegar("/Informacionpersonal")}
-                />
+          <div className="row " >
+            <div className="col ">
+              <Tooltip target=".Matricula2" position="top" mouseTrack mouseTrackLeft={10}>
+                <label>Sistema de matrícula de estudiantes</label>
+              </Tooltip>
+
+              <Card
+                className="Matricula2"
+                id="Matricula"
+                style={{ width: '20em' }}
+                header={<img alt="Card" src={Matricula} />}
+                onClick={() => navegar("/Informacionpersonal")}>
                 <h3>Matrícula</h3>
-              </div>
+              </Card>
             </div>
-            <div className="col-sm">
-              <div id="Constancia">
-                <img
-                  src={Constancia}
-                  alt="Constancias"
-                  width="80%"
-                  onClick={() => navegar("/Constancias")}
-                />
-                <h3>Generar constancia</h3>
-              </div>
+            <div className="col">
+              <Tooltip target=".Constancia" position="top" mouseTrack mouseTrackLeft={10}>
+                <label>Sistema de generación de constancias de traslado y de estudiantes</label>
+              </Tooltip>
+
+              <Card
+                className="Constancia"
+                id="Constancia"
+                style={{ width: '20em' }}
+                header={<img alt="Card" src={Constancia} />}
+                onClick={() => navegar("/Constancias")}>
+                <h3>Generar Constancia</h3>
+              </Card>
+
             </div>
-            <div className="col-sm">
-              <div id="Asistencia">
-                <img
-                  src={Asistencia}
-                  alt="Asistencia"
-                  width="80%"
-                  onClick={() => navegar("/AsistenciaEstudiantes")}
-                />
+
+            <div className="col">
+              <Tooltip target=".Asistencia" position="top" mouseTrack mouseTrackLeft={10}>
+                <label>Sistema de control de asistencia de los estudiantes en las aulas</label>
+              </Tooltip>
+
+              <Card
+                className="Asistencia"
+                id="Asistencia"
+                style={{ width: '20em' }}
+                header={<img alt="Card" src={Asistencia} />}
+                onClick={() => navegar("/AsistenciaEstudiantes")}>
                 <h3>Control de asistencia</h3>
-              </div>
+              </Card>
             </div>
           </div>
-          <div className="row justify-content-md-center">
-            <div className="col-sm">
-              <div id="Reporte">
-                <img src={Reporte} alt="Escuela Rodrigo Facio Brenes"  width="80%"
-                  onClick={() => navegar("/Reporte")}
-                />
+          <div className="row ">
+            <div className="col">
+              <Tooltip target=".Reportes" position="bottom" mouseTrack mouseTrackLeft={10}>
+                <label>Sistema de generación de reportes de asistencia</label>
+              </Tooltip>
+
+              <Card
+                className="Reportes"
+                id="Reporte"
+                style={{ width: '20em' }}
+                header={<img alt="Card" src={Reporte} />}
+                onClick={() => navegar("/Reporte")}>
                 <h3>Generar reporte</h3>
-              </div>
+              </Card>
             </div>
-            <div className="col-sm">
-              <div id="ReporteComedor">
-                <img src={ReporteComedor} alt="Escuela Rodrigo Facio Brenes"  width="80%"
-                  onClick={() => navegar("/ReporteComedor")}
-                />
-                <h3>Generar reporte de comedor</h3>
-              </div>
+            <div className="col">
+              <Tooltip target=".ReporteComedor" position="bottom" mouseTrack mouseTrackLeft={10}>
+                <label>Sistema de generación de reportes de asistencia al comedor</label>
+              </Tooltip>
+
+              <Card
+                className="ReporteComedor"
+                id="ReporteComedor"
+                style={{ width: '20em' }}
+                header={<img alt="Card" src={ReporteComedor} />}
+                onClick={() => navegar("/ReporteComedor")}>
+                <h3>Reporte de comedor</h3>
+              </Card>
             </div>
-            <div className="col-sm">
+            <div className="col">
               <VerEle></VerEle>
             </div>
           </div>
